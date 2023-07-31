286cbf9968c254a558884d62b9c2690b63825774
Deepthireddy56 <bobbadeepthi86@gmail.com>
2023-07-31 13:26:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def loop(request):
+    d={'Name':'DEEPTHI REDDY','Course':['PYTHON','DJANGO','SQL','WEB TECHNOLOGY']}
+    return render(request,'loop.html',context=d)
+def Url(request):
+    d={'A':[1,2,3,4,5,6,7,8],'B':'FRAMEWORK'}
+    return render(request,'Url.html',context=d)
